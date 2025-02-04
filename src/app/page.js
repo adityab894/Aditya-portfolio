c773a8e891d1f244929ed303a017e11488ee64c5
Aditya Bisht <adityabisht894@gmail.com>
2025-02-04 20:33:45 +0530

    fixxxxx

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,8 @@ import Image from "next/image";
 import bg from "../../public/background/home-background.png"
 import Wizard from "./components/models/wizard";
 import Navigation from "./components/navigation";
-import RenderModel from "./components/RenderModel";
+import RenderModel from "@/components/RenderModel";
 
-// import RenderModel from "./components/RenderModel";
 
 export default function Home() {
   return (
