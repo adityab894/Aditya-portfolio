2514a8e6ce7331287e59bf896c82b91a79e9ce5c
Aditya Bisht <adityabisht894@gmail.com>
2025-02-04 19:16:15 +0530

    fixing

--- src/app/(subpages)/projects/page.js
+++ src/app/(subpages)/projects/page.js
@@ -10,19 +10,4 @@ export default function Home() {
       <ProjectList projects={projectsd} />
     </div>
   );
-}
-
-
-
-
-// import Image from "next/image";
-// import bg from "../../../../public/background/home-background.png";
-// import RenderModel from "../../components/rendermodel";
-// import Staff from "../../components/models/staff";
-
-// {/* Staff Animation */}
-// <div className="absolute bottom-10 right-10 z-50">
-// <RenderModel>
-//   <Staff />
-// </RenderModel>
-// </div>
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import bg from "../../public/background/home-background.png"
 import Wizard from "./components/models/wizard";
