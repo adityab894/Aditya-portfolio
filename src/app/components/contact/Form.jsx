33e73e284d62565577915516e3175ec9a728f4b5
Aditya Bisht <adityabisht894@gmail.com>
2025-02-04 10:15:02 +0530

    update

--- src/app/components/contact/Form.jsx
+++ src/app/components/contact/Form.jsx
@@ -5,6 +5,30 @@ import { useForm } from 'react-hook-form';
 
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(process.env.NEXT_PUBLIC_SERVICE_ID, 
+        process.env.NEXT_PUBLIC_TEMPLATE_ID, 
+      
+      {
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        limitRate:{
+          throttle: 5000,
+        }
+      }
+  )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   const onSubmit = data => console.log(data);
   console.log(errors);
   
@@ -18,14 +42,16 @@ export default function Form() {
       <input type="email" placeholder="email" {...register("email", {required: true})} 
         className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
       />
-      <input type="number" placeholder="mobilenumber" {...register("mobilenumber", {required: true})} 
+      <input type="tel" placeholder="mobilenumber" {...register("mobilenumber", {required: true, max: 10, min: 10})} 
         className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
       />
       <textarea {...register("message", {required:true, maxLength: 250, minLength: 40})} 
         className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
       />
 
-      <input type="submit" />
+      <input 
+      value="Drop Your Message"
+      className = 'px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize' type="submit" />
     </form>
   );  
 }
